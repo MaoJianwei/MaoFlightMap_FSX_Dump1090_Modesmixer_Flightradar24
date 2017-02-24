63b92b1e4cf7d7fc2505d83635aa99d1280088d1
Mao Jianwei <maojianwei2020@gmail.com>
2017-02-24 20:37:21 +0800

    autopilot backup

--- SimConnect_API_Usage_Test/QtGuiApplication1/QtGuiApplication1.cpp
+++ SimConnect_API_Usage_Test/QtGuiApplication1/QtGuiApplication1.cpp
@@ -38,39 +38,57 @@ void QtGuiApplication1::killConnect()
 }
 void QtGuiApplication1::getCommanderData()
 {
-	HRESULT hr = SimConnect_RequestDataOnSimObjectType(
-		hSimConnect, CommanderFlightDataReq, CommanderFlightData, 0, SIMCONNECT_SIMOBJECT_TYPE_USER);
+	HRESULT hr = SimConnect_RequestDataOnSimObject(hSimConnect, CommanderFlightDataReq, CommanderFlightData, ui.createObjectId->text().toULong(), SIMCONNECT_PERIOD_ONCE, 0, 0, 0, 0);
+	//HRESULT hr = SimConnect_RequestDataOnSimObjectType(
+	//	hSimConnect, CommanderFlightDataReq, CommanderFlightData, 0, SIMCONNECT_SIMOBJECT_TYPE_USER);
 	
 	handleSimConnectData(hSimConnect); // SIMCONNECT_RECV_ID_SIMOBJECT_DATA_BYTYPE
 }
 void QtGuiApplication1::createOneAI()
 {
 	SIMCONNECT_DATA_INITPOSITION initPos;
-	initPos.Altitude = 1000.7014;
+	initPos.Altitude = 6000.7014;
 	initPos.Latitude = 19.9347;
 	initPos.Longitude = 110.4438;
 	initPos.Pitch = 0; // ¸©Ñö
 	initPos.Bank = 0; // ²àÇã
 	initPos.Heading = 90.0;
 	initPos.OnGround = 0;
-	initPos.Airspeed = 160;
+	initPos.Airspeed = 30;
 
 	HRESULT hr = SimConnect_AICreateNonATCAircraft(hSimConnect, "Boeing 737-800 Paint1", "M-1080", initPos, CreateAiAircraftReq);
 
 	handleSimConnectData(hSimConnect); // SIMCONNECT_RECV_ID_ASSIGNED_OBJECT_ID
 
+	//ui.createObjectId->text().toULong()
+	//hr = SimConnect_TransmitClientEvent(hSimConnect, ui.createObjectId->text().toULong(), Mao_AP_MASTER, 0, SIMCONNECT_GROUP_PRIORITY_DEFAULT, SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
+	hr = SimConnect_TransmitClientEvent(hSimConnect, ui.createObjectId->text().toULong(), Mao_AUTOPILOT_ON, 0, SIMCONNECT_GROUP_PRIORITY_DEFAULT, SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
+	hr = SimConnect_TransmitClientEvent(hSimConnect, ui.createObjectId->text().toULong(), Mao_AP_ATT_HOLD_ON, 0, SIMCONNECT_GROUP_PRIORITY_DEFAULT, SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
+	hr = SimConnect_TransmitClientEvent(hSimConnect, ui.createObjectId->text().toULong(), Mao_AP_HDG_HOLD_ON, 0, SIMCONNECT_GROUP_PRIORITY_DEFAULT, SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
+	hr = SimConnect_TransmitClientEvent(hSimConnect, ui.createObjectId->text().toULong(), Mao_AUTO_THROTTLE_ARM, 1, SIMCONNECT_GROUP_PRIORITY_DEFAULT, SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
+	hr = SimConnect_TransmitClientEvent(hSimConnect, ui.createObjectId->text().toULong(), Mao_AP_PANEL_SPEED_HOLD_TOGGLE, 0, SIMCONNECT_GROUP_PRIORITY_DEFAULT, SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
+	hr = SimConnect_TransmitClientEvent(hSimConnect, ui.createObjectId->text().toULong(), Mao_AP_PANEL_ALTITUDE_SET, 0, SIMCONNECT_GROUP_PRIORITY_DEFAULT, SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
+
 	//hr = SimConnect_TransmitClientEvent(hSimConnect, ui.createObjectId->text().toULong(), Mao_FREEZE_ALTITUDE_SET, 1, SIMCONNECT_GROUP_PRIORITY_DEFAULT, SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
-	hr = SimConnect_TransmitClientEvent(hSimConnect, ui.createObjectId->text().toULong(), Mao_FREEZE_ATTITUDE_SET, 1, SIMCONNECT_GROUP_PRIORITY_DEFAULT, SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
+	//hr = SimConnect_TransmitClientEvent(hSimConnect, ui.createObjectId->text().toULong(), Mao_FREEZE_ATTITUDE_SET, 1, SIMCONNECT_GROUP_PRIORITY_DEFAULT, SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
 	// NO RETURN when success
 }
 void QtGuiApplication1::setAiData()
 {
-	double data = ui.setAlt->text().toDouble();
-	ui.setAlt->setText(QString("%1").arg(data + 0.1));
-	double data2 = ui.setLat->text().toDouble();
-	ui.setLat->setText(QString("%1").arg(data2 - 0.1));
-	HRESULT hr = SimConnect_SetDataOnSimObject(hSimConnect, AircraftBankData, ui.createObjectId->text().toULong(), 0, 0, sizeof(data), &data);
-	hr = SimConnect_SetDataOnSimObject(hSimConnect, AircraftPitchData, ui.createObjectId->text().toULong(), 0, 0, sizeof(data2), &data2);
+	//test pitch
+
+	int data = ui.setAlt->text().toInt();
+	//ui.setAlt->setText(QString("%1").arg(data + 0.1));
+	double data2 = ui.setLat->text().toLong();
+	//ui.setLat->setText(QString("%1").arg(data2 - 0.1));
+	HRESULT hr = SimConnect_SetDataOnSimObject(hSimConnect, AircraftAirSpeedData, ui.createObjectId->text().toInt(), 0, 0, sizeof(data), &data);
+	//HRESULT hr = SimConnect_SetDataOnSimObject(hSimConnect, AircraftAltitudeData, SIMCONNECT_OBJECT_ID_USER, 0, 0, sizeof(data), &data);
+	
+	hr = SimConnect_TransmitClientEvent(hSimConnect, ui.createObjectId->text().toULong(), Mao_AP_ALT_VAR_SET_ENGLISH, data2, SIMCONNECT_GROUP_PRIORITY_DEFAULT, SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
+	hr = SimConnect_TransmitClientEvent(hSimConnect, ui.createObjectId->text().toULong(), Mao_AP_VS_VAR_SET_ENGLISH, 6000, SIMCONNECT_GROUP_PRIORITY_DEFAULT, SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
+	hr = SimConnect_TransmitClientEvent(hSimConnect, ui.createObjectId->text().toULong(), Mao_AP_PANEL_SPEED_HOLD_TOGGLE, 0, SIMCONNECT_GROUP_PRIORITY_DEFAULT, SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
+	hr = SimConnect_TransmitClientEvent(hSimConnect, ui.createObjectId->text().toULong(), Mao_HEADING_BUG_SET, 220, SIMCONNECT_GROUP_PRIORITY_DEFAULT, SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
+	//hr = SimConnect_TransmitClientEvent(hSimConnect, ui.createObjectId->text().toULong(), Mao_AP_ALT_HOLD_ON, 1, SIMCONNECT_GROUP_PRIORITY_DEFAULT, SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
 
 	return;
 
@@ -96,12 +114,29 @@ bool QtGuiApplication1::initSimConnect()
 {
 	HRESULT hr = S_OK;
 	
-	hr = S_OK == hr ? SimConnect_AddToDataDefinition(hSimConnect, CommanderFlightData, "Plane Altitude", "feet") : hr ;
-	hr = S_OK == hr ? SimConnect_AddToDataDefinition(hSimConnect, CommanderFlightData, "Plane Latitude", "degrees") : hr;
-	hr = S_OK == hr ? SimConnect_AddToDataDefinition(hSimConnect, CommanderFlightData, "Plane Longitude", "degrees") : hr;
-	hr = S_OK == hr ? SimConnect_AddToDataDefinition(hSimConnect, CommanderFlightData, "GPS POSITION ALT", "Meters") : hr;
-	hr = S_OK == hr ? SimConnect_AddToDataDefinition(hSimConnect, CommanderFlightData, "GPS POSITION LAT", "degrees") : hr;
-	hr = S_OK == hr ? SimConnect_AddToDataDefinition(hSimConnect, CommanderFlightData, "GPS POSITION LON", "degrees") : hr;
+	//hr = S_OK == hr ? SimConnect_AddToDataDefinition(hSimConnect, CommanderFlightData, "Plane Altitude", "feet") : hr ;
+	//hr = S_OK == hr ? SimConnect_AddToDataDefinition(hSimConnect, CommanderFlightData, "Plane Latitude", "degrees") : hr;
+	//hr = S_OK == hr ? SimConnect_AddToDataDefinition(hSimConnect, CommanderFlightData, "Plane Longitude", "degrees") : hr;
+	//hr = S_OK == hr ? SimConnect_AddToDataDefinition(hSimConnect, CommanderFlightData, "GPS POSITION ALT", "Meters") : hr;
+	//hr = S_OK == hr ? SimConnect_AddToDataDefinition(hSimConnect, CommanderFlightData, "GPS POSITION LAT", "degrees") : hr;
+	//hr = S_OK == hr ? SimConnect_AddToDataDefinition(hSimConnect, CommanderFlightData, "GPS POSITION LON", "degrees") : hr;
+
+
+	hr = S_OK == hr ? SimConnect_AddToDataDefinition(hSimConnect, CommanderFlightData, "AUTOPILOT AVAILABLE", "Bool") : hr;
+	hr = S_OK == hr ? SimConnect_AddToDataDefinition(hSimConnect, CommanderFlightData, "AUTOPILOT MASTER", "Bool") : hr;
+	hr = S_OK == hr ? SimConnect_AddToDataDefinition(hSimConnect, CommanderFlightData, "AUTOPILOT WING LEVELER", "Bool") : hr;
+	hr = S_OK == hr ? SimConnect_AddToDataDefinition(hSimConnect, CommanderFlightData, "AUTOPILOT HEADING LOCK", "Bool") : hr;
+	hr = S_OK == hr ? SimConnect_AddToDataDefinition(hSimConnect, CommanderFlightData, "AUTOPILOT HEADING LOCK DIR", "Degrees") : hr;
+	hr = S_OK == hr ? SimConnect_AddToDataDefinition(hSimConnect, CommanderFlightData, "AUTOPILOT ALTITUDE LOCK", "Bool") : hr;
+	hr = S_OK == hr ? SimConnect_AddToDataDefinition(hSimConnect, CommanderFlightData, "AUTOPILOT ALTITUDE LOCK VAR", "Feet") : hr;
+	hr = S_OK == hr ? SimConnect_AddToDataDefinition(hSimConnect, CommanderFlightData, "AUTOPILOT ATTITUDE HOLD", "Bool") : hr;
+	hr = S_OK == hr ? SimConnect_AddToDataDefinition(hSimConnect, CommanderFlightData, "AUTOPILOT PITCH HOLD REF", "degrees") : hr;
+	hr = S_OK == hr ? SimConnect_AddToDataDefinition(hSimConnect, CommanderFlightData, "AUTOPILOT VERTICAL HOLD", "Bool") : hr;
+	hr = S_OK == hr ? SimConnect_AddToDataDefinition(hSimConnect, CommanderFlightData, "AUTOPILOT VERTICAL HOLD VAR", "Feet/minute") : hr;
+	hr = S_OK == hr ? SimConnect_AddToDataDefinition(hSimConnect, CommanderFlightData, "AUTOPILOT AIRSPEED HOLD", "Bool") : hr;
+	hr = S_OK == hr ? SimConnect_AddToDataDefinition(hSimConnect, CommanderFlightData, "AUTOPILOT AIRSPEED HOLD VAR", "Knots") : hr;
+	hr = S_OK == hr ? SimConnect_AddToDataDefinition(hSimConnect, CommanderFlightData, "AUTOPILOT THROTTLE ARM", "Bool") : hr;
+
 
 	hr = S_OK == hr ? SimConnect_AddToDataDefinition(hSimConnect, AiFlightData, "Initial Position", NULL, SIMCONNECT_DATATYPE_INITPOSITION) : hr;
 
@@ -116,9 +151,19 @@ bool QtGuiApplication1::initSimConnect()
 	//PLANE PITCH DEGREES
 	//PLANE BANK DEGREES
 
-	
 	hr = SimConnect_MapClientEventToSimEvent(hSimConnect, Mao_FREEZE_ALTITUDE_SET, "FREEZE_ALTITUDE_SET");
 	hr = SimConnect_MapClientEventToSimEvent(hSimConnect, Mao_FREEZE_ATTITUDE_SET, "FREEZE_ATTITUDE_SET");
+	hr = SimConnect_MapClientEventToSimEvent(hSimConnect, Mao_AP_MASTER, "AP_MASTER");
+	hr = SimConnect_MapClientEventToSimEvent(hSimConnect, Mao_AUTOPILOT_ON, "AUTOPILOT_ON");
+	hr = SimConnect_MapClientEventToSimEvent(hSimConnect, Mao_AP_ATT_HOLD_ON, "AP_ATT_HOLD_ON");
+	hr = SimConnect_MapClientEventToSimEvent(hSimConnect, Mao_AP_HDG_HOLD_ON, "AP_HDG_HOLD_ON");
+	hr = SimConnect_MapClientEventToSimEvent(hSimConnect, Mao_HEADING_BUG_SET, "HEADING_BUG_SET");
+	hr = SimConnect_MapClientEventToSimEvent(hSimConnect, Mao_AP_PANEL_ALTITUDE_SET, "AP_PANEL_ALTITUDE_SET");
+	hr = SimConnect_MapClientEventToSimEvent(hSimConnect, Mao_AP_PANEL_ALTITUDE_HOLD, "AP_PANEL_ALTITUDE_HOLD");
+	hr = SimConnect_MapClientEventToSimEvent(hSimConnect, Mao_AP_ALT_VAR_SET_ENGLISH, "AP_ALT_VAR_SET_ENGLISH");
+	hr = SimConnect_MapClientEventToSimEvent(hSimConnect, Mao_AP_PANEL_SPEED_HOLD_TOGGLE, "AP_PANEL_SPEED_HOLD_TOGGLE");
+	hr = SimConnect_MapClientEventToSimEvent(hSimConnect, Mao_AUTO_THROTTLE_ARM, "AUTO_THROTTLE_ARM");
+	hr = SimConnect_MapClientEventToSimEvent(hSimConnect, Mao_AP_VS_VAR_SET_ENGLISH, "AP_VS_VAR_SET_ENGLISH");
 
 
 	// NO RETURN when success
@@ -151,9 +196,11 @@ void QtGuiApplication1::_handleSimConnectData(SIMCONNECT_RECV* pData)
 	{
 		case SIMCONNECT_RECV_ID_OPEN:
 			break;
+		case SIMCONNECT_RECV_ID_SIMOBJECT_DATA:
 		case SIMCONNECT_RECV_ID_SIMOBJECT_DATA_BYTYPE:
 		{
-			SIMCONNECT_RECV_SIMOBJECT_DATA_BYTYPE *pObjData = (SIMCONNECT_RECV_SIMOBJECT_DATA_BYTYPE*)pData;
+			//SIMCONNECT_RECV_SIMOBJECT_DATA_BYTYPE *pObjData = (SIMCONNECT_RECV_SIMOBJECT_DATA_BYTYPE*)pData;
+			SIMCONNECT_RECV_SIMOBJECT_DATA *pObjData = (SIMCONNECT_RECV_SIMOBJECT_DATA*)pData;
 
 			switch (pObjData->dwRequestID)
 			{
@@ -161,15 +208,29 @@ void QtGuiApplication1::_handleSimConnectData(SIMCONNECT_RECV* pData)
 				{
 					CommanderFlightDataStruct *pCFD = (CommanderFlightDataStruct*)&pObjData->dwData;
 					ui.commanderData->setPlainText(
-						QString("\nObjectID=%1  \nplaneAlt=%2  planeLat=%3  planeLon=%4  \ngpsAlt=%5  gpsLat=%6  gpsLon=%7  \ncalculateAlt=%8")
-						.arg(pObjData->dwObjectID)
-						.arg(pCFD->planeAlt)
-						.arg(pCFD->planeLat)
-						.arg(pCFD->planeLon)
-						.arg(pCFD->gpsAlt)
-						.arg(pCFD->gpsLat)
-						.arg(pCFD->gpsLon)
-						.arg(pCFD->gpsAlt * 3.28084));
+						QString("AVAILABLE=%1  \nMASTER=%2  \nWING LEVELER=%3  \nHEADING LOCK=%4  \nHEADING LOCK DIR=%5  \nALTITUDE LOCK=%6  \nALTITUDE LOCK VAR=%7  \nATTITUDE HOLD=%8  \nPITCH HOLD REF=%9  \nVERTICAL HOLD=%10  \nVERTICAL HOLD VAR=%11  \nAIRSPEED HOLD=%12  \nAIRSPEED HOLD VAR=%13  \nTHROTTLE ARM=%14")
+						//.arg(pObjData->dwObjectID)
+						//.arg(pCFD->planeAlt)
+						//.arg(pCFD->planeLat)
+						//.arg(pCFD->planeLon)
+						//.arg(pCFD->gpsAlt)
+						//.arg(pCFD->gpsLat)
+						//.arg(pCFD->gpsLon)
+						//.arg(pCFD->gpsAlt * 3.28084));
+						.arg(pCFD->a1)
+						.arg(pCFD->a2)
+						.arg(pCFD->a3)
+						.arg(pCFD->a4)
+						.arg(pCFD->a5)
+						.arg(pCFD->a6)
+						.arg(pCFD->a7)
+						.arg(pCFD->a8)
+						.arg(pCFD->a9)
+						.arg(pCFD->a10)
+						.arg(pCFD->a11)
+						.arg(pCFD->a12)
+						.arg(pCFD->a13)
+						.arg(pCFD->a14));
 					break;
 				}
 			}
--- SimConnect_API_Usage_Test/QtGuiApplication1/QtGuiApplication1.h
+++ SimConnect_API_Usage_Test/QtGuiApplication1/QtGuiApplication1.h
@@ -37,7 +37,20 @@ private:
 
 enum Mao_EVENT_ID {
 	Mao_FREEZE_ALTITUDE_SET,
-	Mao_FREEZE_ATTITUDE_SET
+	Mao_FREEZE_ATTITUDE_SET,
+
+	Mao_AP_MASTER,
+	Mao_AUTOPILOT_ON,
+	Mao_AP_ATT_HOLD_ON, // pitch and so on...
+	Mao_AP_HDG_HOLD_ON,
+	Mao_AP_PANEL_HEADING_HOLD,
+	Mao_HEADING_BUG_SET,
+	Mao_AP_PANEL_ALTITUDE_SET,
+	Mao_AP_PANEL_ALTITUDE_HOLD,
+	Mao_AP_ALT_VAR_SET_ENGLISH,
+	Mao_AP_PANEL_SPEED_HOLD_TOGGLE,
+	Mao_AUTO_THROTTLE_ARM,
+	Mao_AP_VS_VAR_SET_ENGLISH,
 };
 
 enum Mao_DATA_DEFINE_ID {
@@ -62,13 +75,28 @@ enum Mao_DATA_REQUEST_ID {
 
 struct CommanderFlightDataStruct
 {
-	double  planeAlt;
-	double  planeLat;
-	double  planeLon;
+	//double  planeAlt;
+	//double  planeLat;
+	//double  planeLon;
 
-	double  gpsAlt;
-	double  gpsLat;
-	double  gpsLon;
+	//double  gpsAlt;
+	//double  gpsLat;
+	//double  gpsLon;
+
+	double  a1;
+	double  a2;
+	double  a3;
+	double  a4;
+	double  a5;
+	double  a6;
+	double  a7;
+	double  a8;
+	double  a9;
+	double  a10;
+	double  a11;
+	double  a12;
+	double  a13;
+	double  a14;
 };
 
 //struct AircraftAltitudeStruct {
